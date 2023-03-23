85b30df43c2e12fb8520c2e7c8fea8a74ddd43f5
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-03-23 19:32:49 +0530

    Animation update and Who Added and You added

--- public/client.js
+++ public/client.js
@@ -15,7 +15,6 @@ if (!userName || !nameRegex.test(userName)) {
 
 
 let textArea = document.querySelector(".text_input")
-
 textArea.focus()
 
 
@@ -23,33 +22,33 @@ let messageArea = document.querySelector(".message_area")
 
 
 
+let who = null
 
 
 
 
 textArea.addEventListener("keyup", (e) => {
 
-
     if (e.key === "Enter") {
-
         submitMessage()
 
     }
-
 })
 
 
 
 
+// // // This is actual  function to send msg -->
+
 function submitMessage() {
     let value = textArea.value.trim()
-
     // console.log(e.target.value)
     if (value === '') {
         return alert("Write something in Message Box, please")
     }
     else {
         sendMessage(value)
+        who = 0
     }
 }
 
@@ -73,9 +72,7 @@ function sendMessage(msg) {
     scollToBottom()
 
     // // Sending to server ------>
-
     socket.emit("message", msgObj)
-
 
 }
 
@@ -85,20 +82,58 @@ function sendMessage(msg) {
 
 function appentMsg(msgObj, type) {
 
+
     let mainDiv = document.createElement("div")
 
-    let className = type
+    // // // All classes ------->
+    mainDiv.classList.add(type, 'msg', "animate__animated", "animate__flipInY")
 
-    mainDiv.classList.add(className, 'msg')
 
-    let markUp = `
-    <h5>${msgObj.user}</h5>
-    <p>${msgObj.message}</p>
-    `
+    let markUp;
+
+
+    if (who === null) {
+
+        if (type === "out") {
+            markUp = `
+            <h5>You (${msgObj.user})</h5>
+            <p>${msgObj.message}</p>
+            `
+        } else {
+            markUp = `
+            <h5>${msgObj.user}</h5>
+            <p>${msgObj.message}</p>
+            `
+        }
+
+    } else if (who === 0 && type === "out") {
+
+        markUp = `
+        <p>${msgObj.message}</p>
+        `
+    } else if (who === 1 && type === "in") {
+
+        markUp = `
+        <p>${msgObj.message}</p>
+        `
+    } else {
+
+        if (type === "out") {
+            markUp = `
+            <h5>You (${msgObj.user})</h5>
+            <p>${msgObj.message}</p>
+            `
+        }else{
+            markUp = `
+            <h5>${msgObj.user}</h5>
+            <p>${msgObj.message}</p>
+            `
+        }
+
+    }
 
 
     mainDiv.innerHTML = markUp
-
     messageArea.appendChild(mainDiv)
 
 }
@@ -109,25 +144,15 @@ function appentMsg(msgObj, type) {
 // // Reciving msgs -------->
 socket.on('message', (msgObj) => {
 
-    // let actualObj = JSON.parse(msgObj)
-
-    // console.log(actualObj)
 
     if (Object.keys(msgObj).length === 1) {
-
-        // console.log("ok")
-        // console.log(actualObj)
-
         document.querySelector(".online").innerHTML = `<h3 class="text-warning">${msgObj.online} Online</h3>`
-
-
-
     } else {
 
         appentMsg(msgObj, "in")
         scollToBottom()
-
-
+        who = 1
+        // console.log(who)
     }
 
 })
